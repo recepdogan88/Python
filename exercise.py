9986a0d109a252b072840934fe45104cb5860cad
Recep Dogan <rdogan8891@gmail.com>
2023-06-07 23:48:28 +0200

    get a specific number in numpy array

---
+++ exercise.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+b = np.array([[9.0,8.0,7.0],[6.0,5.0,4.0]])
+print(b.size)
+
+# Get a specific element [r, c]
+a = np.array([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14]])
+print(a)
+print(a[1, 5])
